76dade3537c931a5cc50812a9d22730bf10b97db
omeribrahimov24 <165696859+omeribrahimov24@users.noreply.github.com>
2024-04-07 13:45:56 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,9 +1,14 @@
 #import libraries here
 
-def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+def main(): 
+vowel=["a","e","i","o","u"]
+a=input("Enter a letter of the alphabet")
+if a=="y":
+    print("Sometimes it is a vowel, and sometimes it is a consonant!")
+elif a in vowel:
+    print("Entered alphabet is a vowel!")
+else:
+    print("Entered alphabet is a consonant!")
   pass
 
 if __name__ == "__main__":
